97e0629a57886d9e10f3a543f5dd90ac71254cbb
Brendan Sluke <contact@brendansluke.com>
2022-06-26 13:40:56 -0500

    refactor(blog): lint blog template

--- brendansluke/pages/blogs/[id].tsx
+++ brendansluke/pages/blogs/[id].tsx
@@ -1,46 +1,46 @@
-import Layout from '../../components/layout'
-import Head from 'next/head'
-import { Blog, getAllBlogIds, getBlogData } from '../../lib/blogs'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
-import { GetStaticProps } from 'next/types'
+import Layout from "../../components/layout";
+import Head from "next/head";
+import { Blog, getAllBlogIds, getBlogData } from "../../lib/blogs";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps } from "next/types";
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const blogData = await getBlogData(context.params?.id as string)
-    return {
-      props: {
-        blogData
-      }
-    }
-}
+  const blogData = await getBlogData(context.params?.id as string);
+  return {
+    props: {
+      blogData,
+    },
+  };
+};
 
 export async function getStaticPaths() {
-    const paths = getAllBlogIds()
-    return {
-      paths,
-      fallback: false
-    }
-  }
+  const paths = getAllBlogIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 interface BlogProps {
-    blogData: Blog
+  blogData: Blog;
 }
 
 export default function BlogPage({ blogData }: BlogProps) {
-    return (
-      <Layout home>
-        {/* Add this <Head> tag */}
-        <Head>
-            <title>{blogData.title}</title>
-        </Head>
+  return (
+    <Layout home>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{blogData.title}</title>
+      </Head>
 
-        <article>
-            <h1 className={utilStyles.headingXl}>{blogData.title}</h1>
-            <div className={utilStyles.lightText}>
-            <Date dateString={blogData.date} />
-            </div>
-            <div dangerouslySetInnerHTML={{ __html: blogData.contentHtml }} />
-        </article>
-      </Layout>
-    )
-  }
+      <article>
+        <h1 className={utilStyles.headingXl}>{blogData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={blogData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: blogData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
